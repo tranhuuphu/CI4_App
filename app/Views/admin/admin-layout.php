55a58fa8244833ec47d0d161a6eca938aef15aab
tranhuuphu <tranhuuphu9x@gmail.com>
2022-12-11 00:40:55 +0700

    #10 File

--- app/Views/admin/admin-layout.php
+++ app/Views/admin/admin-layout.php
@@ -120,23 +120,45 @@
 
 	</script> -->
 
+	<!-- <script type="text/javascript">
+
+		tinymce.init({
+    selector: "textarea#content",theme: "modern",width: 680,height: 300,
+    plugins: [
+         "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+         "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
+         "table contextmenu directionality emoticons paste textcolor responsivefilemanager code"
+   ],
+   toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect",
+   toolbar2: "| responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+   image_advtab: true ,
+   
+   external_filemanager_path:"/filemanager/",
+   filemanager_title:"Responsive Filemanager" ,
+   external_plugins: { "filemanager" : "/filemanager/plugin.min.js"}
+ });
+
+
+	</script> -->
+
 	<script>
     var editor_config = {
       path_absolute : "<?= base_url('/admin'); ?>/",
       selector: 'textarea#content',
       height: 500,
       relative_urls: false,
+      remove_script_host: false,
 
       oninit : "setPlainText",
 
-      plugins: 'print preview importcss searchreplace autolink autosave save directionality  visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap emoticons template paste',
+      plugins: 'print preview importcss searchreplace autolink autosave save directionality  visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap emoticons template paste responsivefilemanager',
       fontsize_formats: '8px 10px 12px 14px 16px 18px 24px 36px 48px 72px',
       
       menubar: 'file edit view insert format tools table tc',
       default_link_target: '_blank',
       
 
-      toolbar: 'undo redo | formatselect fontselect fontsizeselect | bold italic underline strikethrough image toc |  alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | media template link anchor codesample',
+      toolbar: 'undo redo | formatselect fontselect fontsizeselect | bold italic underline strikethrough image toc |  alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | media template link anchor codesample | responsivefilemanager',
       toolbar_sticky: true,
       autosave_ask_before_unload: true,
       autosave_interval: '30s',
@@ -157,75 +179,19 @@
 		    images_upload_url: 'postAcceptor.php',
 		    here we add custom filepicker only to Image dialog
 		  */
-		  file_picker_types: 'image',
-		  /* and here's our custom image picker*/
-		  file_picker_callback: function (cb, value, meta) {
-		    var input = document.createElement('input');
-		    input.setAttribute('type', 'file');
-		    input.setAttribute('accept', 'image/*');
-
-		    /*
-		      Note: In modern browsers input[type="file"] is functional without
-		      even adding it to the DOM, but that might not be the case in some older
-		      or quirky browsers like IE, so you might want to add it to the DOM
-		      just in case, and visually hide it. And do not forget do remove it
-		      once you do not need it anymore.
-		    */
-
-		    input.onchange = function () {
-		      var file = this.files[0];
-
-		      var reader = new FileReader();
-		      reader.onload = function () {
-		        /*
-		          Note: Now we need to register the blob in TinyMCEs image blob
-		          registry. In the next release this part hopefully won't be
-		          necessary, as we are looking to handle it internally.
-		        */
-		        var id = 'blobid' + (new Date()).getTime();
-		        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-		        var base64 = reader.result.split(',')[1];
-		        var blobInfo = blobCache.create(id, file, base64);
-		        blobCache.add(blobInfo);
-
-		        /* call the callback and populate the Title field with the file name */
-		        cb(blobInfo.blobUri(), { title: file.name });
-		      };
-		      reader.readAsDataURL(file);
-		    };
-
-		    input.click();
-		  },
-
+		  image_advtab: true ,
+   
+			external_filemanager_path:"<?= base_url('public/admin_asset/responsive_filemanager'); ?>/filemanager/",
+			filemanager_title:"Responsive Filemanager" ,
+			external_plugins: { "filemanager" : "<?= base_url('public/admin_asset/responsive_filemanager'); ?>/filemanager/plugin.min.js"},
+			filemanager_access_key:"myPrivateKey" ,
 
       content_style: ".mce-content-body {font-size:16px;font-family: Andale Mono;}",
       // image_uploadtab: true,
       contextmenu_never_use_native: true,
       
 
-      file_picker_callback : function(callback, value, meta) {
-        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-        // var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
-        // if (meta.filetype == 'image') {
-        //   cmsURL = cmsURL + "&type=Images";
-        // } else {
-        //   cmsURL = cmsURL + "&type=Files";
-        // }
-
-        tinyMCE.activeEditor.windowManager.openUrl({
-          url : cmsURL,
-          title : 'Filemanager',
-          width : x * 0.8,
-          height : y * 0.8,
-          resizable : "yes",
-          close_previous : "no",
-          onMessage: (api, message) => {
-            callback(message.content);
-          }
-        });
-      }
+      
 
     };
 
